2c85aabffe766f0abaea49a83a8697c5af1252e7
Simon Whitaker <sw@netcetera.org>
2021-12-11 12:47:28 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Introduction
+
+On 24 May, 2020, a rogue staffer posted a [highly critical tweet](https://www.theguardian.com/politics/2020/may/24/can-you-imagine-having-to-work-with-these-truth-twisters) on the UK Civil Service twitter account. Ever since, [@ThatCivilTweet](https://twitter.com/ThatCivilTweet) has been tweeting the same text twice a day, every day, to keep it fresh in our minds.
+
+The code in this repository queries the Twitter API for the public stats (e.g. like counts) for every tweet from the @ThatCivilTweet account, then offers a Jupyter Notebook for visualising the data in an interactive plot.
+
+# Getting started
+
+To run the code, first register for the [Twitter API](https://developer.twitter.com/en/docs/platform-overview) and generate a bearer token, then export it to your environment:
+
+```
+export TWITTER_API_TOKEN=<your bearer token here>
+```
+
+Now run the code, ideally inside a virtual environment.
+
+```
+pip install -r requirements.txt
+python main.py > data.csv
+```
+
+Now open viz.ipynb and run it to generate an interactive plot. If you've never used a .ipynb file before, the easiest way to get started is to open it in VS Code with the [Python extension](https://marketplace.visualstudio.com/items?itemName=ms-python.python) installed.
---
+++ main.py
@@ -0,0 +1,48 @@
+import json
+import os
+import requests
+
+expected_content = (
+    "Arrogant and offensive. Can you imagine having to work with these truth twisters?"
+)
+account_id = 1264862081301520384  # @ThatCivilTweet
+url = f"https://api.twitter.com/2/users/{account_id}/tweets"
+token = os.environ.get("TWITTER_API_TOKEN")
+headers = {"Authorization": f"Bearer {token}"}
+
+# CSV header
+print("id,timestamp,likes,retweets")
+
+# until_id
+earliest_seen_tweet_id = None
+data = []
+
+querystring = {"tweet.fields": "public_metrics,created_at", "max_results": "100"}
+
+while earliest_seen_tweet_id is None or len(data) < 100:
+    if earliest_seen_tweet_id:
+        querystring["until_id"] = earliest_seen_tweet_id
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    data = json.loads(response.text)
+    tweets = data.get("data", [])
+    if len(tweets) == 0:
+        break
+
+    earliest_seen_tweet_id = tweets[-1]["id"]
+
+    # Filter out any tweets that aren't the usual content, e.g. replies,
+    # retweets, etc
+    tweets = filter(lambda t: t["text"] == expected_content, tweets)
+
+    for tweet in tweets:
+        print(
+            ",".join(
+                [
+                    tweet["id"],
+                    tweet["created_at"],
+                    str(tweet["public_metrics"]["like_count"]),
+                    str(tweet["public_metrics"]["retweet_count"]),
+                ]
+            )
+        )
